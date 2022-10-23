577869b645c84348c7b29ea80ca9807a8a27ede5
paithoonchatbot <72309054+paithoonchatbot@users.noreply.github.com>
2022-10-23 12:32:46 +0700

    Add files via upload

--- Speaker.py
+++ Speaker.py
@@ -5,6 +5,14 @@ while 1:
     print("Input Word :")
     s=input()
     speaker.Speak(s)
+    
+"""Procedure in Crate
+*1. pip install pypiwin32
+*2. Write in code above
+*3.Run with type: python Speaker.py
+*4.Input Word : occurs
+*5. Type word, Sentense, or Paragraph
+"""
 
 
 
