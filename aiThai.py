4ff914465ce44ce97da2f81bc60e510b314f50cb
paithoonchatbot <72309054+paithoonchatbot@users.noreply.github.com>
2022-10-27 22:11:23 +0700

    Add files via upload

--- aiThai.py
+++ aiThai.py
@@ -1,7 +1,10 @@
+from expertai.nlapi.cloud.client import ExpertAiClient
 from sklearn import tree
+client = ExpertAiClient()
 
 features = [[115, 70], [180, 100], [159, 99], [139, 89], [120, 80]]
-labels = ["STORNGE /NORMAL", "HYPERTENSION /WEAKNESS", "HYPERTENSION WEAK", "WEAKNESS", "NORMAL "]
+labels = ["STORNGE /NORMAL", "HYPERTENSION /WEAKNESS",
+          "HYPERTENSION WEAK", "WEAKNESS", "NORMAL "]
 classifier = tree.DecisionTreeClassifier()
 classifier = classifier.fit(features, labels)
 
